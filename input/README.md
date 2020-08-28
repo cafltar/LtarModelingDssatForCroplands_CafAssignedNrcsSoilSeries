df54e4eeca91a9fff43ec6f6e617031f1d55f7c6
Bryan Carlson <bryan.r.carlson@gmail.com>
2020-08-28 10:05:20 -0700

    init

---
+++ input/README.md
@@ -0,0 +1,28 @@
+# Files
+
+## 08/24/2020
+
+* [Cunningham Farm - Soil Series Approximations.xls](Cunningham%20Farm%20-%20Soil%20Series%20Approximations.xls)
+  * From Bryan's Google My Drive: [G:\My Drive\Archive\NasRm243_20160615_oldDataBackup\DataDriveBackup\Projects\Individual_Users\Mark_Wardell\Excel_Files]
+  * Potential source of "soil-survey-map-caf.png" as sent by David Huggins via email "RE: Cook Soil series" on 01/29/2020
+* [Wardell Soil Map.jpg](Wardell%20Soil%20Map.jpg)
+  * From Bryan's Google My Drive: [G:\My Drive\Archive\NasRm243_20160615_oldDataBackup\DataDriveBackup\Projects\Individual_Users\Bruce_Frazier]
+  * Alternative soil series map from "soil-survey-map-caf.png" as sent by David Huggins via email "RE: Cook Soil series" on 01/29/2020
+* [Wardell Soil Series.xls](Wardell Soil Series.xls)
+  * From Bryan's Google My Drive: [G:\My Drive\Archive\NasRm243_20160615_oldDataBackup\DataDriveBackup\Projects\Cunningham_Farm\Baseline\Grid_Sampling\Points]
+  * Looks to be duplicate of [Cunningham Farm - Soil Series Approximations.xls], last modified Aug 19, 2009
+* [Wardell Soil Series.jpg](Wardell Soil Series.jpg)
+  * From Bryan's Google My Drive: [G:\My Drive\Archive\NasRm243_20160615_oldDataBackup\DataDriveBackup\Projects\Cunningham_Farm\2009\Soil Oranic Carbon\SOC Pictures\SOC Pictures NOT Final]
+  * Many similar maps all over Drive - these do not match "soil-survey-map-caf.png" as sent by David Huggins via email "RE: Cook Soil series" on 01/29/2020
+
+## 08/25/2020
+
+* [cookeast_georeferencepoint_20190924.geojson](cookeast_georeferencepoint_20190924.geojson)
+  * From: C:\Dev\Projects\CafGeospatialBoundariesAndGridPoints\CleanAndStandardize\output, Repo: https://github.com/cafltar/CafGeospatialBoundariesAndGridPoints_CleanAndStandardize
+  
+## 08/28/2020
+
+* [CAF_soil_type.xls](CAF_soil_type.xls)
+  * From Bryan's Google My Drive: [G:\My Drive\Archive\DaveHuggins_20160801_oldDataBackup\CookFarm\CAF_Ped]
+  * Dave Huggins says: "...it has Wardell’s description, my classification and then the soil survey classification. So, yes, column E is what Jeff should use."
+  * Dave reclassified the Unknown and Buried/Altered to others
---
+++ src/clean-check-input.py
@@ -0,0 +1,122 @@
+import pathlib
+import pandas as pd
+import geopandas as gpd
+from shapely import ops
+import datetime
+
+def assign_series_names(df:pd.DataFrame):
+    df_out = df.copy()
+
+    df_out["SeriesName"] = ""
+
+    df_out.loc[(df["Series"] == 1), "SeriesName"] = "Caldwell"
+    df_out.loc[(df["Series"] == 2), "SeriesName"] = "Unknown"
+    df_out.loc[(df["Series"] == 3), "SeriesName"] = "Latah"
+    df_out.loc[(df["Series"] == 4), "SeriesName"] = "Naff"
+    df_out.loc[(df["Series"] == 5), "SeriesName"] = "Naff"
+    df_out.loc[(df["Series"] == 6), "SeriesName"] = "Palouse"
+    df_out.loc[(df["Series"] == 7), "SeriesName"] = "Palouse"
+    df_out.loc[(df["Series"] == 8), "SeriesName"] = "Staley"
+    df_out.loc[(df["Series"] == 9), "SeriesName"] = "Thatuna"
+    df_out.loc[(df["Series"] == 10), "SeriesName"] = "Buried/Altered"
+
+    return df_out
+
+def convert_coord_to_wgs84(df:pd.DataFrame):
+    df_gpd = gpd.GeoDataFrame(
+        df.copy(), 
+        crs = "EPSG:32611",
+        geometry=gpd.points_from_xy(df.Easting, df.Northing))
+    
+    df_gpd = df_gpd.to_crs("EPSG:4326")
+    df_gpd = df_gpd.assign(
+        Latitude = df_gpd.geometry.y,
+        Longitude = df_gpd.geometry.x)
+
+    df_out = (df.copy()
+        .assign(
+            Latitude = df_gpd.Latitude,
+            Longitude = df_gpd.Longitude
+        ))
+
+    return df_out
+
+def assign_id2_by_nearest_neighbor(
+    df:pd.DataFrame, 
+    grid_points:gpd.GeoDataFrame):
+    # find the nearest point and return the corresponding ID2 value
+    # From: https://gis.stackexchange.com/a/222388
+
+    df_gpd = gpd.GeoDataFrame(
+        df.copy(), 
+        crs = "EPSG:4326",
+        geometry=gpd.points_from_xy(df.Longitude, df.Latitude))
+
+    pts = grid_points.unary_union
+
+    df_gpd["ID2"] = df_gpd.apply(lambda x: near(grid_points, x.geometry, pts), axis = 1)
+
+    df_out = pd.DataFrame(df_gpd)
+    return df_out
+
+def near(grid_points, point, pts):
+     # find the nearest point and return the corresponding Place value
+     # From: https://gis.stackexchange.com/a/222388
+     nearest = grid_points.geometry == ops.nearest_points(point, pts)[1]
+     result = grid_points[nearest]["ID2"].values[0]
+     return result
+
+
+
+def main(
+    pathSoiSeries: pathlib.Path,
+    pathGeorefPoints: pathlib.Path,
+    outputDir: pathlib.Path
+    ):
+    ## Data Transformation
+    df_in = pd.read_excel(
+        pathSoiSeries, 
+        sheet_name="Revised Series",
+        usecols=[0,1,2,6],
+        names=[
+            "ID",
+            "Easting",
+            "Northing",
+            "Series"
+        ],
+        converters={
+            "ID":int, 
+            "Easting":float, 
+            "Northing":float, 
+            "Series":int})
+    
+    georefPointsIn = gpd.read_file(pathGeorefPoints)
+
+    df = (df_in
+        .pipe(assign_series_names)
+        .pipe(convert_coord_to_wgs84)
+        .pipe(assign_id2_by_nearest_neighbor, georefPointsIn)
+        .sort_values(by="ID2")
+    )
+
+    # Write files
+    date_today = datetime.datetime.now().strftime("%Y%m%d")
+
+    df[["ID2", "SeriesName"]].to_csv(
+        outputDir / "CookEastNrcsSoilSeries_{}_P1A1.csv".format(date_today),
+        index = False)
+
+
+
+if __name__ == "__main__":
+    # params
+    inputDir = pathlib.Path.cwd() / "input"
+    inputWardellSoilSeries = inputDir / "Wardell Soil Series.xls"
+    inputPathGeorefPoints = inputDir / "cookeast_georeferencepoint_20190924.geojson"
+    outputDir = pathlib.Path.cwd() / "output"
+
+    main(
+        inputWardellSoilSeries,
+        inputPathGeorefPoints,
+        outputDir
+    )
